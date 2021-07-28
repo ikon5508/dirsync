bd47b2321cfbe53cf4e3870f9c98d231f4f15605
ikon5508 <42330231+ikon5508@users.noreply.github.com>
2021-07-27 22:29:25 -0700

    Add files via upload

--- logging.c
+++ logging.c
@@ -14,108 +14,77 @@ va_start (ap, format);
 
 
 int formatlen = strlen (format);
-
-int count = 0;
-int modulo = 0;
 char type;
-char entry [maxbuffer] = "";
-char part [maxbuffer];
-int oldmodulo = 0;
+char specify_len = 0;
+char entry [string_sz] = "";
+//char part [maxbuffer] = "";
 
-
-int len;
-
+int len = 0;
 
 int d;
 char c;
 char *s;
 
-do {
-oldmodulo = modulo;
 
-if (modulo > 0)
-	modulo = getnext (format, '%', modulo + 1, formatlen);
-
-if (modulo == 0)
-	{modulo = getnext (format, '%', 0, formatlen);
-	/*printf ("first-%d\n", modulo);*/}
-
-if (modulo > -1)
+for (int fplace = 0; fplace < formatlen; ++fplace)
 {
-++count;
-type = format [modulo + 1];
+if (format [fplace] == '%')
+{
 
-	if(oldmodulo > 0)
-	{
-	midstr (format, part, oldmodulo + 2, modulo);
-	strcat (entry, part);
-	
-	} // if oldmodulo > 0
-
-	if(oldmodulo == 0)
-	{
-	midstr (format, part, 0, modulo);
-	strcat (entry, part);
-	printf ("first-%d\n", modulo);
-	} // if oldmodulo > 0
-
-
+type = format [fplace + 1];
+    
 
 if (type == 's')
 {
 s = va_arg(ap, char *);
-strcat (entry, s);
+
+//int slen = strlen (s);
+
+len += sprintf (entry + len, "%s", s);
+++fplace;
+
 
 } // if s
-
+    
 if (type == 'd')
 {
 d = va_arg(ap, int);
-sprintf (part, "%d", d);
-strcat (entry, part);
-
+len += sprintf (entry + len, "%d", d);
+++fplace;
 } // if d
-
+    
 
 if (type == 'c')
 {
 c = (char) va_arg(ap, int);
-//strcat (entry, c);
-len = strlen (entry);
 entry [len] = c;
+++len;
+entry [len] = 0;
+++fplace;    
 } // if c
+    
+    
+}else{ // else if    
+entry [len] = format [fplace];
+++len;
 
-
-//printf ("%d: %s\n", count, part);
-
-
-} // if
-} while (modulo > 0);
+} // if    
+} // for
 
 //printf ("%s\n", entry);
 
 va_end(ap);
 
-len = strlen (format);
-
-midstr (format, part, oldmodulo + 2, len);
-strcat (entry, part);
-len = strlen (entry);
-
 write (fd, entry, len);
-
 
 } // loggingf
 
 
-
-int close_log ()
+void close_log ()
 {
-close (fd);
-
+ close (fd);
+ 
 }
-
-
 
 int init_log (const char *path)
 {
